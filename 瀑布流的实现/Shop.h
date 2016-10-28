bcfc4c37e1d0052d06d40efcee7b85b6755e2252
ZJ <xuan.xue.ai@qq.com>
2016-10-28 16:54:18 +0800

    解决一些文件丢失

---
+++ 瀑布流的实现/Shop.h
@@ -0,0 +1,18 @@
+//
+//  Shop.h
+//  瀑布流的实现
+//
+//  Created by 周君 on 16/10/28.
+//  Copyright © 2016年 周君. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Shop : NSObject
+
+@property (nonatomic, assign) CGFloat w;
+@property (nonatomic, assign) CGFloat h;
+@property (nonatomic, copy) NSString *img;
+@property (nonatomic, copy) NSString *price;
+
+@end
---
+++ 瀑布流的实现/Shop.m
@@ -0,0 +1,13 @@
+//
+//  Shop.m
+//  瀑布流的实现
+//
+//  Created by 周君 on 16/10/28.
+//  Copyright © 2016年 周君. All rights reserved.
+//
+
+#import "Shop.h"
+
+@implementation Shop
+
+@end
